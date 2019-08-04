5b4566e9257add46771e95784f219a0802d6f7b8
Casper Neo <casperneo@uchicago.edu>
2019-08-03 20:00:58 -0700

    Obj stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,76 @@
+#![feature(dbg_macro)]
+
 extern crate flatbuffers;
 
 mod monster_generated;
 mod monster_obj;
 
-use flatbuffers::FlatBufferBuilder;
+use crate::monster_generated::my_game::sample::{get_root_as_monster,
+                                             Color, Equipment,
+                                             Monster, MonsterArgs,
+                                             Vec3,
+                                             Weapon, WeaponArgs};
+
+fn make_monster() -> (Vec<u8>, usize) {
+    let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
+    let weapon_one_name = builder.create_string("Sword");
+    let weapon_two_name = builder.create_string("Axe");
+    // Use the `Weapon::create` shortcut to create Weapons with named field
+    // arguments.
+    let sword = Weapon::create(&mut builder, &WeaponArgs{
+        name: Some(weapon_one_name),
+        damage: 3,
+    });
+    let axe = Weapon::create(&mut builder, &WeaponArgs{
+        name: Some(weapon_two_name),
+        damage: 5,
+    });
+    // Name of the Monster.
+    let name = builder.create_string("Orc");
+    // Inventory.
+    let inventory = builder.create_vector(&[0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    // Create a FlatBuffer `vector` that contains offsets to the sword and axe
+    // we created above.
+    let weapons = builder.create_vector(&[sword, axe]);
+    // Create the monster using the `Monster::create` helper function. This
+    // function accepts a `MonsterArgs` struct, which supplies all of the data
+    // needed to build a `Monster`. To supply empty/default fields, just use the
+    // Rust built-in `Default::default()` function, as demonstrated below.
+
+    // Create the path vector of Vec3 objects.
+    let x = Vec3::new(1.0, 2.0, 3.0);
+    let y = Vec3::new(4.0, 5.0, 6.0);
+    let path = builder.create_vector(&[x, y]);
+    // Note that, for convenience, it is also valid to create a vector of
+    // references to structs, like this:
+    // let path = builder.create_vector(&[&x, &y]);
+
+    let orc = Monster::create(&mut builder, &MonsterArgs{
+        pos: Some(&Vec3::new(1.0f32, 2.0f32, 3.0f32)),
+        mana: 150,
+        hp: 80,
+        name: Some(name),
+        inventory: Some(inventory),
+        color: Color::Red,
+        weapons: Some(weapons),
+        equipped_type: Equipment::Weapon,
+        equipped: Some(axe.as_union_value()),
+        path: Some(path),
+    });
+    builder.finish(orc, None);
+    builder.collapse()
+}
+
+
 
 fn main() {
     println!("Hello, world!");
-    let _bldr = FlatBufferBuilder::new();
-    let _bytes: Vec<u8> = Vec::new();
+    let (buf, root) = make_monster();
+    let monster = get_root_as_monster(&buf[root..]);
+    dbg!(monster.hp());
+    dbg!(monster.mana());
+    dbg!(monster.pos());
+
+    dbg!(monster.unpack());
+
 }
--- src/monster_obj.rs
+++ src/monster_obj.rs
@@ -1,33 +1,45 @@
 // Manual implementation of the Rust object API.
 // For learning and eventual testing purposes.
 
-// flatbuffer-enums and flatbuffer-structs can share types with the builder
-// api but tables and unions need to be generated to remove WOffset stuff.
+// flatbuffer-enums can share types with the builder, maybe. Tables and unions need to be
+// generated to remove WOffset stuff. Even structs because of endianness
 use crate::monster_generated::my_game::sample::Color;
+use crate::monster_generated::my_game::sample::Equipment;
 use crate::monster_generated::my_game::sample::Monster;
 use crate::monster_generated::my_game::sample::Vec3;
-use flatbuffers;
+use crate::monster_generated::my_game::sample::Weapon;
 
+// TODO Other derives.
+#[derive(Debug)]
+pub struct Vec3T {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(Debug)]
 pub struct WeaponT {
-    name: String,
+    name: Option<String>,
     damage: i16,
 }
 
+#[derive(Debug)]
 pub enum EquipmentT {
-    None,
+    NONE,
     WeaponT(WeaponT),
 }
 
+#[derive(Debug)]
 pub struct MonsterT {
-    pub pos: Option<Vec3>,
+    pub pos: Option<Vec3T>,
     pub mana: i16,
     pub hp: i16,
-    pub name: String,
+    pub name: Option<String>,
     pub inventory: Vec<u8>,
     pub color: Color,
     pub weapons: Vec<WeaponT>,
     pub equipped: EquipmentT,
-    pub path: Vec<Vec3>,
+    pub path: Vec<Vec3T>,
 }
 
 impl Default for MonsterT {
@@ -36,19 +48,56 @@ impl Default for MonsterT {
             pos: None,
             mana: 150,
             hp: 100,
-            name: String::new(),
+            name: None,
             inventory: Vec::new(),
             color: Color::Blue,
             weapons: Vec::new(),
-            equipped: EquipmentT::None,
+            equipped: EquipmentT::NONE,
             path: Vec::new(),
+        }
+    }
+}
+
+impl Vec3 {
+    pub fn unpack(&self) -> Vec3T {
+        Vec3T {
+            x: self.x(),
+            y: self.y(),
+            z: self.z(),
+        }
+    }
+}
+
+impl<'a> Weapon<'a> {
+    pub fn unpack(&self) -> WeaponT {
+        WeaponT {
+            name: self.name().map(|n| n.to_string()),
+            damage: self.damage()
         }
     }
 }
 
 impl<'a> Monster<'a> {
     pub fn unpack(&self) -> MonsterT {
-        unimplemented!()
+        MonsterT {
+            pos: self.pos().map(|x| x.unpack()), // Stuct => unpack.
+            mana: self.mana(),
+            hp: self.hp(),
+            name: self.name().map(|x| x.to_string()),
+            inventory: self.inventory().unwrap_or(&[]).to_vec(), // Vec of scalars is simple to_vec
+            color: self.color(),
+            weapons: match self.weapons() { // Vector of tables
+                None => Vec::new(),
+                Some(w) => (0..w.len()).map(|i| w.get(i).unpack()).collect(),
+            },
+            equipped: match self.equipped_type() { // Union type.
+                Equipment::NONE => EquipmentT::NONE,
+                // This breaks references
+                Equipment::Weapon => EquipmentT::WeaponT(self.equipped_as_weapon().unwrap().unpack()),
+            },
+            // Vector of structs needs unpack.
+            path: self.path().unwrap_or(&[]).iter().map(|x| x.unpack()).collect(),
+        }
     }
     pub fn pack(&mut self, m: &MonsterT) {
         unimplemented!()
